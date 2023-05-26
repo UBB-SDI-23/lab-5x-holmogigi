fe758a6d2d55795c4615f3bbf4cffc03cf412583
holmogigi <nicholassteam@yahoo.com>
2023-05-26 22:33:46 +0300

    AI added on API
    epic

---
+++ MPP_holmogigi/Controllers/WeightPredictionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.AspNetCore.Authorization;
+using System.IO.Compression;
+using Org.BouncyCastle.Utilities.Zlib;
+using static MPP.MLModel;
+
+namespace MPP.Controllers
+{
+    [Route("api/AiModel")]
+    [ApiController]
+    public class WeightPredictionController : ControllerBase
+    {
+        public class WeightPredictionData
+        {
+            [ColumnName("division")]
+            public string Division { get; set; }
+
+            [ColumnName("age")]
+            public int Age { get; set; }
+        }
+
+        // Define a class to hold the prediction result
+        public class WeightPredictionResult
+        {
+            public int Prediction { get; set; }
+        }
+
+        [HttpGet("{division}/{age}/{height}")]
+        [AllowAnonymous]
+        public IActionResult GetWeightPrediction(string division, int age, int height)
+        {
+            // Load sample data
+            var sampleData = new MLModel.ModelInput()
+            {
+                Division = division,
+                Age = age,
+                Height = height
+            };
+
+            int weight = (int)MLModel.Predict(sampleData).Score;
+
+            return Ok(weight);
+        }
+    }
+}
